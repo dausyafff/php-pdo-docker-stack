a806a0761ec531d54bab654318e18eccb8f06c7b
Muhammad Dausyaf Aryandha <muhammaddausyaf@gmail.com>
2026-03-06 15:02:04 +0700

    update model produk

--- app/model/Produk.php
+++ app/model/Produk.php
@@ -10,11 +10,9 @@ class Produk
 
   public function readAll()
   {
-    // Query Relasi: Menampilkan produk dan nama kategori terkait
-    $query = "SELECT p.id, p.nama_produk, k.nama_kategori 
-                  FROM produk p 
-                  LEFT JOIN kategori k ON p.kategori_id = k.id 
-                  ORDER BY p.id DESC";
+    $query = "SELECT no, nama_barang 
+              FROM produk 
+              ORDER BY no DESC";
 
     $stmt = $this->conn->prepare($query);
     $stmt->execute();
