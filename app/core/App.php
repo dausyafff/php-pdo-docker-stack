2eee411f08447ac36e37132a05590f944863f9d6
Muhammad Dausyaf Aryandha <muhammaddausyaf@gmail.com>
2026-03-18 12:40:16 +0700

    app url setting

--- app/core/App.php
+++ app/core/App.php
@@ -1,2 +1,42 @@
 <?php
-class App {}
+class App
+{
+  protected $controller = 'home';
+  protected $method = 'index';
+  protected $params = [];
+
+  public function __construct()
+  {
+    $url = $this->parseUrl();
+
+    // Cek apakah controller ada
+    if (file_exists('../app/controllers/' . $url[0] . '.php')) {
+      $this->controller = $url[0];
+      unset($url[0]);
+    }
+
+    require_once '../app/controllers/' . $this->controller . '.php';
+    $this->controller = new $this->controller;
+
+    // Cek apakah method ada
+    if (isset($url[1])) {
+      if (method_exists($this->controller, $url[1])) {
+        $this->method = $url[1];
+        unset($url[1]);
+      }
+    }
+
+    // Ambil parameter
+    $this->params = $url ? array_values($url) : [];
+
+    // Jalankan controller dan method dengan parameter
+    call_user_func_array([$this->controller, $this->method], $this->params);
+  }
+
+  public function parseUrl()
+  {
+    if (isset($_GET['url'])) {
+      return explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+    }
+  }
+}
